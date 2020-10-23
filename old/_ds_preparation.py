b0ebbcf67ac03c913812cf94e5a51ca1a7d08231
Marco <marcocolu@hotmail.it>
2020-10-23 14:56:09 +0200

    removed old

--- old/_ds_preparation.py
+++
@@ -1,6 +0,0 @@
-import split_folders
-
-# Split with a ratio.
-# To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
-split_folders.ratio('./urban/test/', output="./urban/DS/", seed=1337, ratio=(.7, .2, .1)) # default values
-
